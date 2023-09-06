df4044c0364d9d14c1ef1225b850fe39725b158b
Gabriel Carvalho <gabriel225gc@gmail.com>
2023-09-06 15:53:30 -0300

    componente header pronto

---
+++ src/components/header/index.tsx
@@ -0,0 +1,31 @@
+import Logo from '/logo-gc.png';
+import styles from './Header.module.scss';
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <img 
+        src={Logo} 
+        alt="logo na cor branca escrito gc entre o sinal de maior que e menor que com barra no final" 
+        className={styles.header__logo}
+      />
+
+      <nav className={styles.container}>
+        <ul className={styles.container__lista}>
+          <li className={styles.container__item}>
+            <a href="#" className={styles.container__link}>Home</a>
+          </li>
+          <li className={styles.container__item}>
+            <a href="#" className={styles.container__link}>Sobre mim</a>
+          </li>
+          <li className={styles.container__item}>
+            <a href="#" className={styles.container__link}>Projetos</a>
+          </li>
+          <li className={styles.container__item}>
+            <a href="#" className={styles.container__link}>Contato</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
