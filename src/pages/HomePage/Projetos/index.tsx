fff1c5d4c883e4feda5380828ba351e4492bd196
Gabriel Carvalho <gabriel225gc@gmail.com>
2023-09-12 18:02:21 -0300

    código dos cards prontos

--- src/pages/HomePage/Projetos/index.tsx
+++ src/pages/HomePage/Projetos/index.tsx
@@ -1,74 +1,79 @@
-import React, { useState } from 'react';
-import { motion, useAnimation } from 'framer-motion';
-import projectsData from './cards.json'; // Importe os dados do arquivo JSON
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import cards from './cards.json';
+import styles from './Projetos.module.scss';
 
-import styles from './Projetos.module.scss'; // Certifique-se de importar seus estilos
-
-interface Project {
+interface Card {
   id: number;
   img: string;
   titulo: string;
   descricao: string;
+  detalhe: string;
+  recursos: {
+    tecnologia: string;
+    outros: string;
+  };
+  botoes: {
+    projeto: string;
+    codigo: string;
+  };
 }
 
-const Projetos: React.FC = () => {
-  const [showCard, setShowCard] = useState<boolean>(false);
-  const [selectedProject, setSelectedProject] = useState<Project | null>(null);
+const Projetos = () => {
+  const [cardStates, setCardStates] = useState<{ [key: number]: boolean }>({});
 
-  const controls = useAnimation();
-
-  const toggleCard = (item: Project) => {
-    setSelectedProject(item);
-    setShowCard(!showCard);
-
-    // Animação do card usando framer-motion
-    controls.start({
-      opacity: showCard ? 0 : 1,
-      scale: showCard ? 0 : 1,
-    });
+  const toggleCard = (id: number) => {
+    setCardStates((prevCardStates) => ({
+      ...prevCardStates,
+      [id]: !prevCardStates[id],
+    }));
   };
-
-  const cards: Project[] = projectsData; // Use os dados do arquivo JSON
 
   return (
     <section className={styles.secao}>
       <h2 className={styles.secao__titulo}>Projetos</h2>
 
-      {cards.map((item: Project) => (
-        <div className={styles.projeto} key={item.id}>
-          <img
-            src={item.img}
-            alt={item.titulo}
-            className={styles.projeto__img}
-          />
-
-          <div className={styles.projeto__container}>
-            <h3 className={styles.projeto__container__titulo}>{item.titulo}</h3>
-
-            <p className={styles.projeto__container__descricao}>{item.descricao}</p>
-
-            <button
-              className={styles.projeto__container__botao}
-              onClick={() => toggleCard(item)}
-            >
-              Ver mais
-            </button>
-          </div>
-        </div>
-      ))}
-
-      {showCard && selectedProject && (
+      {cards.map((item: Card) => (
         <motion.div
-          initial={{ opacity: 0, scale: 0 }}
-          animate={controls}
-          exit={{ opacity: 0, scale: 0 }}
-          transition={{ duration: 0.5 }}
-          className={`${styles.card} ${styles.cardVisible}`} // Adicione a classe cardVisible para mostrar o card
+          className={styles.card}
+          key={item.id}
+          onClick={() => toggleCard(item.id)}
+          layout="size"
+          transition={{ duration: .6, type: "spring" }}
         >
-          <h2>{selectedProject.titulo}</h2>
-          <p>{selectedProject.descricao}</p>
+          <motion.img src={item.img} alt={item.titulo} className={styles.card__img} layout/>
+
+          <div className={styles.card__container}>
+            <h3 className={styles.card__container__titulo}>{item.titulo}</h3>
+
+            <p className={styles.card__container__descricao}>{item.descricao}</p>
+
+
+
+            {cardStates[item.id] && (
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 1 }}
+                className={styles.card__info}
+              >
+                <p className={styles.card__info__paragrafo}>{item.detalhe}</p>
+                <p className={styles.card__info__paragrafo}>{item.recursos.tecnologia}</p>
+                <p className={styles.card__info__paragrafo}>{item.recursos.outros}</p>
+                <div className={styles.card__info__links}>
+                  <a href={item.botoes.projeto}>Ver Projeto</a>
+                  <a href={item.botoes.codigo}>Ver código</a>
+                </div>
+              </motion.div>
+            )}
+            {!cardStates[item.id] &&(
+              <p className={styles.card__container__verMais}>Clique no card para Ver mais</p>
+            )
+            }
+
+          </div>
         </motion.div>
-      )}
+      ))}
     </section>
   );
 };
