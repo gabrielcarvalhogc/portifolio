b2e20762efbfa33df2322d1f92432dc461e44995
Gabriel Carvalho <gabriel225gc@gmail.com>
2023-12-06 17:29:32 -0300

    feat: new projects data array

---
+++ src/data/projetcs-data.ts
@@ -0,0 +1,32 @@
+export const ProjectsData = [
+  {
+    "id": 1,
+    "img": "/thumb-capputeeno.png",
+    "title": "Capputeeno",
+    "description": "Este projeto de e-commerce, desenvolvido com Next.js e React usando TypeScript e Styled Components,se aproveita do consumo de uma API GraphQL para fornecer dados dinâmicos. A experiência de compra é aprimorada pela integração direta com essa API, garantindo informações atualizadas e uma jornada de compra mais fluida e personalizada.",
+    "links": {
+      "projectLink": "https://capputeeno-omega.vercel.app/",
+      "githubLink": "https://github.com/gabrielcarvalhogc/capputeeno"
+    }
+  },
+  {
+    "id": 2,
+    "img": "/thumb-casa-da-massa.png",
+    "title": "Casa da massa",
+    "description": "Este projeto, desenvolvido com Vite, React (utilizando TypeScript e SASS), concentra-se principalmente no uso das bibliotecas react-dom e react-router-dom para a criação e gestão de rotas. A combinação dessas poderosas ferramentas permite uma navegação fluida e dinâmica dentro da aplicação. Além de aprimorar a funcionalidade e a aparência, a utilização estratégica dessas bibliotecas proporciona uma experiência de usuário altamente interativa.",
+    "links": {
+      "projectLink": "https://casa-da-massa.vercel.app/",
+      "githubLink": "https://github.com/gabrielcarvalhogc/Cardapio"
+    }
+  },
+  {
+    "id": 3,
+    "img": "/thumb-sneakers-store.png",
+    "title": "Sneakers Store",
+    "description": "Este projeto de e-commerce, desenvolvido com Next.js, React (usando TypeScript e SCSS Module), prioriza o uso de testes unitários. Com o auxílio das bibliotecas Jest e Testing Library, cada componente e funcionalidade é cuidadosamente testado, assegurando a confiabilidade e a consistência do código.",
+    "links": {
+      "projectLink": "https://sneakers-ecommerce-sand.vercel.app/",
+      "githubLink": "https://github.com/gabrielcarvalhogc/sneakers-ecommerce"
+    }
+  }
+];
