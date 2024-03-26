4208f2e11b666315c082bbef13a2a901b99f076c
Gabriel Carvalho <gabriel225gc@gmail.com>
2024-03-26 19:44:29 -0300

    Chore: Changed footer images to icons

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,39 +1,27 @@
-import logoTelefone from '/info/logo-telefone.png';
-import logoGmail from '/info/logo-gmail.png';
-import logoLinkedin from '/info/logo-linkedin.png';
 import styles from './Footer.module.scss';
+import ContactPhoneIcon from '@mui/icons-material/ContactPhone';
+import EmailIcon from '@mui/icons-material/Email';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 export default function Footer() {
   return (
     <footer className={styles.rodape} id='contato'>
-      <div className={styles.rodape__container}>
-        <img 
-          className={styles.rodape__container__img} 
-          src={logoTelefone} 
-          alt="Icone de telefone" 
-        />
-        <p className={styles.rodape__container__info}>+55 (61) 99401 2764</p>
+      <div className={styles.container}>
+        <ContactPhoneIcon fontSize='large'/>
+        <p className={styles.info}>+55 (61) 99401 2764</p>
       </div>
 
-      <div className={styles.rodape__container}>
-        <img 
-          className={styles.rodape__container__img} 
-          src={logoGmail} 
-          alt="Icone do e-mail" 
-        />
-        <p className={styles.rodape__container__info}>gabriel225gc@gmail.com</p>
+      <div className={styles.container}>
+        <EmailIcon fontSize='large'/>
+        <p className={styles.info}>gabriel225gc@gmail.com</p>
       </div>
       <a
-        className={styles.rodape__container}
+        className={styles.container}
         href='https://www.linkedin.com/in/gabrielcarvalhodev/'
         target='_blank'
       >
-        <img 
-          className={styles.rodape__container__img} 
-          src={logoLinkedin} 
-          alt="Icone do LinkedIn" 
-        />
-        <p className={styles.rodape__container__info}>LinkedIn</p>
+        <LinkedInIcon fontSize='large'/>
+        <p className={styles.info}>Gabriel Carvalho</p>
       </a>
     </footer>
   )
