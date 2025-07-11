674e32ab768a66d293129c0ee66dc98a34ddd106
Gabriel Carvalho <gabriel225gc@gmail.com>
2025-07-11 15:33:01 -0300

    feat: enhance toolbar layout and implement menu items to mobile devices with dark mode toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,48 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Toolbar } from 'primeng/toolbar';
 import { ButtonModule } from 'primeng/button';
-import { InputTextModule } from 'primeng/inputtext';
-import { CardModule } from 'primeng/card';
+import { MenuModule } from 'primeng/menu';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Toolbar, ButtonModule, InputTextModule, CardModule],
+  imports: [RouterOutlet, Toolbar, ButtonModule, MenuModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'portifolio';
   isDarkMode = false;
+
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Início',
+        icon: 'pi pi-home',
+        // command: () => this.navigateTo('/inicio') // Exemplo de navegação
+      },
+      {
+        label: 'Sobre',
+        icon: 'pi pi-info-circle',
+      },
+      {
+        label: 'Projetos',
+        icon: 'pi pi-briefcase',
+      },
+      {
+        label: 'Contato',
+        icon: 'pi pi-envelope',
+      },
+      {
+        label: 'Tema',
+        icon: this.isDarkMode ? 'pi pi-sun' : 'pi pi-moon',
+        command: () => this.toggleDarkMode()
+      }
+    ]
+  }
 
   toggleDarkMode() {
     const element = document.querySelector('html');
