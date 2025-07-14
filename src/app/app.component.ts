d61745be431084504664e09120aa4dfe39ad95f1
Gabriel Carvalho <gabriel225gc@gmail.com>
2025-07-14 14:46:15 -0300

    feat: create standalone Toolbar component and integrate into app layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,53 +4,17 @@ import { ButtonModule } from 'primeng/button';
 import { MenuModule } from 'primeng/menu';
 import { MenuItem } from 'primeng/api';
 import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ToolbarComponent } from "./components/toolbar/toolbar.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Toolbar, ButtonModule, MenuModule, AboutMeComponent],
+  imports: [Toolbar, ButtonModule, MenuModule, AboutMeComponent, ToolbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'portifolio';
-  isDarkMode = false;
 
-  items: MenuItem[] | undefined;
-
-  ngOnInit() {
-    this.items = [
-      {
-        label: 'Início',
-        icon: 'pi pi-home',
-        // command: () => this.navigateTo('/inicio') // Exemplo de navegação
-      },
-      {
-        label: 'Sobre',
-        icon: 'pi pi-info-circle',
-      },
-      {
-        label: 'Projetos',
-        icon: 'pi pi-briefcase',
-      },
-      {
-        label: 'Contato',
-        icon: 'pi pi-envelope',
-      },
-      {
-        label: 'Tema',
-        icon: this.isDarkMode ? 'pi pi-sun' : 'pi pi-moon',
-        command: () => this.toggleDarkMode()
-      }
-    ]
-  }
-
-  toggleDarkMode() {
-    const element = document.querySelector('html');
-    if (element !== null) {
-      this.isDarkMode = !this.isDarkMode;
-      element.classList.toggle('my-app-dark');
-    }
-  }
 }
---
+++ src/app/components/toolbar/toolbar.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { MenuModule } from 'primeng/menu';
+import { Toolbar } from 'primeng/toolbar';
+
+@Component({
+  selector: 'app-toolbar',
+  standalone: true,
+  imports: [Toolbar, ButtonModule, MenuModule],
+  templateUrl: './toolbar.component.html',
+  styleUrl: './toolbar.component.scss'
+})
+export class ToolbarComponent {
+  isDarkMode = false;
+
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Início',
+        icon: 'pi pi-home',
+        // command: () => this.navigateTo('/inicio') // Exemplo de navegação
+      },
+      {
+        label: 'Sobre',
+        icon: 'pi pi-info-circle',
+      },
+      {
+        label: 'Projetos',
+        icon: 'pi pi-briefcase',
+      },
+      {
+        label: 'Contato',
+        icon: 'pi pi-envelope',
+      },
+      {
+        label: 'Tema',
+        icon: this.isDarkMode ? 'pi pi-sun' : 'pi pi-moon',
+        command: () => this.toggleDarkMode()
+      }
+    ]
+  }
+
+  toggleDarkMode() {
+    const element = document.querySelector('html');
+    if (element !== null) {
+      this.isDarkMode = !this.isDarkMode;
+      element.classList.toggle('my-app-dark');
+    }
+  }
+}
