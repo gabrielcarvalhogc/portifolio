025f488f29d939694cd52dc61365f8e34cc9058a
Gabriel Carvalho <gabriel225gc@gmail.com>
2025-07-17 16:00:00 -0300

    feat: add Footer component with template and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { AboutMeComponent } from './components/about-me/about-me.component';
 import { ToolbarComponent } from "./components/toolbar/toolbar.component";
 import { SkillsComponent } from './components/skills/skills.component';
 import { ProjectsComponent } from "./components/projects/projects.component";
+import { FooterComponent } from "./components/footer/footer.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AboutMeComponent, ToolbarComponent, SkillsComponent, ProjectsComponent],
+  imports: [AboutMeComponent, ToolbarComponent, SkillsComponent, ProjectsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
